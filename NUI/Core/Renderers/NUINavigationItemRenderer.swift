53a76fb27966cff8e9a6e1773a727057c00efef5
broadwaylamb <seergy_pro@me.com>
2016-06-15 22:06:23 +0300

    #3: Translate NUINavigationItemRenderer into Swift

---
+++ NUI/Core/Renderers/NUINavigationItemRenderer.swift
@@ -0,0 +1,31 @@
+//
+//  NUINavigationItemRenderer.swift
+//  NUIDemo
+//
+//  Created by Sergej Jaskiewicz on 06/15/16.
+//  Copyright © 2016 Tom Benner. All rights reserved.
+//
+
+import UIKit
+
+class NUINavigationItemRenderer: NSObject {
+    
+    static func render(item: UINavigationItem, withClass className: String) {
+        
+        if let backBarButtonItem = item.backBarButtonItem {
+            NUIRenderer.renderBarButtonItem(backBarButtonItem, withClass: className)
+        }
+        
+        if let leftBarButtonItems = item.leftBarButtonItems {
+            for leftBarButtonItem in leftBarButtonItems {
+                NUIRenderer.renderBarButtonItem(leftBarButtonItem, withClass: className)
+            }
+        }
+        
+        if let rightBarButtonItems = item.rightBarButtonItems {
+            for rightBarButtonItem in rightBarButtonItems {
+                NUIRenderer.renderBarButtonItem(rightBarButtonItem, withClass: className)
+            }
+        }
+    }
+}
