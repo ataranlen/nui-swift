f9f8dc60f7561e2db5bf875f7bc3c53ba4d00a69
broadwaylamb <seergy_pro@me.com>
2016-06-21 21:40:22 +0300

    #3: Translate NUISliderRenderer into Swift

---
+++ NUI/Core/Renderers/NUISliderRenderer.swift
@@ -0,0 +1,39 @@
+//
+//  NUISliderRenderer.swift
+//  NUIDemo
+//
+//  Created by Sergej Jaskiewicz on 21.06.16.
+//  Copyright © 2016 Tom Benner. All rights reserved.
+//
+
+import UIKit
+
+class NUISliderRenderer: NSObject {
+    
+    static func render(slider: UISlider, withClass className: String) {
+        
+        if NUISettings.hasProperty("minimum-track-tint-color", withClass: className) {
+            slider.minimumTrackTintColor = NUISettings.getColor("minimum-track-tint-color", withClass: className)
+        }
+        
+        if NUISettings.hasProperty("maximum-track-tint-color", withClass: className) {
+            slider.maximumTrackTintColor = NUISettings.getColor("maximum-track-tint-color", withClass: className)
+        }
+        
+        if NUISettings.hasProperty("minimum-value-image", withClass: className) {
+            slider.minimumValueImage = NUISettings.getImage("minimum-value-image", withClass: className)
+        }
+        
+        if NUISettings.hasProperty("maximum-value-image", withClass: className) {
+            slider.maximumValueImage = NUISettings.getImage("maximum-value-image", withClass: className)
+        }
+        
+        if NUISettings.hasProperty("thumb-image", withClass: className) {
+            slider.setThumbImage(NUISettings.getImage("thumb-image", withClass: className), forState: .Normal)
+        }
+        
+        if NUISettings.hasProperty("thumb-tint-color", withClass: className) {
+            slider.thumbTintColor = NUISettings.getColor("thumb-tint-color", withClass: className)
+        }
+    }
+}
