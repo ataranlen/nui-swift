65a123498db75d7de4eaa63ca7460619528b8fe0
broadwaylamb <seergy_pro@me.com>
2016-06-27 22:00:39 +0300

    #3: Translate NUITextInputTraitsRenderer into Swift

---
+++ NUI/Core/Renderers/NUITextInputTraitsRenderer.swift
@@ -0,0 +1,29 @@
+//
+//  NUITextInputTraitsRenderer.swift
+//  NUIDemo
+//
+//  Created by Sergej Jaskiewicz on 06/27/16.
+//  Copyright © 2016 Tom Benner. All rights reserved.
+//
+
+import UIKit
+
+class NUITextInputTraitsRenderer: NSObject {
+    
+    
+    static func renderKeyboard(traits: UITextInputTraits, withClass className: String) {
+        
+        // FIXME: Swift doesn't let you set properties declared with an `optional` keyword, so downcast is needed.
+        // Couldn't come up with any workaround yet.
+        if NUISettings.hasProperty("keyboard-appearance", withClass: className) {
+            
+            let keyboardAppearance = NUISettings.getKeyboardAppearance("keyboard-appearance", withClass: className)
+            
+            if let traits = traits as? UITextField {
+                traits.keyboardAppearance = keyboardAppearance
+            } else if let traits = traits as? UITextView {
+                traits.keyboardAppearance = keyboardAppearance
+            }
+        }
+    }
+}
