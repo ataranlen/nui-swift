2dad414928681eeb68479dada48a2120d01e532f
broadwaylamb <seergy_pro@me.com>
2016-08-05 01:22:02 +0300

    Attempt solve signing problem

--- Demo/NUITests/UIKitTests/UIBarButton/NUIBarButtonSnapshotTests.swift
+++ Demo/NUITests/UIKitTests/UIBarButton/NUIBarButtonSnapshotTests.swift
@@ -14,15 +14,11 @@ class NUIBarButtonSnapshotTests: FBSnapshotTestCase {
     
     var sut: UIButton!
     
-    let NUIBarButtonGradientTestsStyleClass: String = "BarButtonWithGradientColor"
-    
     override func setUp() {
         super.setUp()
         
         NUISettings.initWithStylesheet("TestTheme.NUI")
         sut = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 26))
-        sut.nuiClass = NUIBarButtonGradientTestsStyleClass
-        sut.applyNUI()
         
         // Flip this to true to record images in the reference image directory.
         recordMode = false
@@ -32,7 +28,9 @@ class NUIBarButtonSnapshotTests: FBSnapshotTestCase {
     
     // background-color-top/background-color-bottom (Gradient)
     func testButtonWithGradient() {
-        FBSnapshotVerifyView(sut, identifier: "")
+        sut.nuiClass = "BarButtonWithGradientColor"
+        sut.applyNUI()
+        FBSnapshotVerifyView(sut)
     }
     
     // border-color (Color)
@@ -41,6 +39,6 @@ class NUIBarButtonSnapshotTests: FBSnapshotTestCase {
     func testButtonWithBorder() {
         sut.nuiClass = "BarButtonWithBorder"
         sut.applyNUI()
-        FBSnapshotVerifyView(sut, identifier: "")
+        FBSnapshotVerifyView(sut)
     }
 }
