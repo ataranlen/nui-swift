c59a5e310c4c8d057f422ad5730a702509ea6c1d
broadwaylamb <seergy_pro@me.com>
2016-08-04 21:57:31 +0300

    #6: Translate NUIActivityIndicatorRendererTests into Swift

---
+++ Demo/NUITests/UIKitTests/NUIActivityIndicatorRendererTests.swift
@@ -0,0 +1,32 @@
+//
+//  NUIActivityIndicatorRendererTests.swift
+//  NUIDemo
+//
+//  Created by Sergej Jaskiewicz on 08/04/16.
+//  Copyright © 2016 Tom Benner. All rights reserved.
+//
+
+import XCTest
+@testable import NUIDemo
+
+class NUIActivityIndicatorRendererTests: XCTestCase {
+    
+    var sut: UIActivityIndicatorView!
+    
+    override func setUp() {
+        super.setUp()
+        NUISettings.initWithStylesheet("TestTheme.NUI")
+        self.sut = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        sut.applyNUI()
+    }
+    
+    override func tearDown() {
+        self.sut = nil
+        super.tearDown()
+    }
+    // color (Color)
+    
+    func testSetColor() {
+        XCTAssertEqual(sut.color, UIColor.red, "NUI should set color activity indicator color")
+    }
+}
